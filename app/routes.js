17aad0268fe6a3263605b3289cdf74790baeef18
robertcao <yuan.cao@sjsu.edu>
2014-11-24 18:50:20 -0800

    Change the default UI when user login

--- app/routes.js
+++ app/routes.js
@@ -352,7 +352,7 @@ module.exports = function (app, passport) {
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/profile-view-only', // redirect to the secure profile section
+        successRedirect: '/postcenter', // redirect to the secure profile section
         failureRedirect: '/login', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
