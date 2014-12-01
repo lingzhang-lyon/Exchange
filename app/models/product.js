262c989cb1884fa7cf0f8b341e00e20b12199645
robertcao <yuan.cao@sjsu.edu>
2014-12-01 13:20:57 -0800

    Add product model

---
+++ app/models/product.js
@@ -0,0 +1,17 @@
+// load the things we need
+var mongoose = require('mongoose');
+
+// define the schema for our post model
+var productSchema = mongoose.Schema({
+	productName		  : String,
+	userId			  : Number,
+	quantity		  : Number,
+	expectedOffer	  : String,
+	productDesc		  : String,
+	productExpiryDate : Date,
+	isValid			  : Number,
+	categoryId		  : Number,
+	lastUpdated		  : Date
+});
+
+module.exports = mongoose.model('Product', pruductSchema);
